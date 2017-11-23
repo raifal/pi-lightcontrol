bfac5bb7f0fc3028dd5bd49020eb63ef14e125ac
Rainer Faller <raifal@users.noreply.github.com>
2017-11-23 21:17:30 +0100

    added logging

--- src/main/java/de/rainerfaller/hsm/pi/lightcontrol/Application.java
+++ src/main/java/de/rainerfaller/hsm/pi/lightcontrol/Application.java
@@ -1,5 +1,8 @@
 package de.rainerfaller.hsm.pi.lightcontrol;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+    private static Logger logger = LoggerFactory.getLogger(Application.class.getName());
+
     public static void main(String[] args) throws Exception {
+        logger.warn("MyMessage");
         SpringApplication.run(new Object[]{Application.class}, args);
     }
 }
